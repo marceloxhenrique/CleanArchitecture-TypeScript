47bd880d5a68155df6b18aa5d0736570db1c29ad
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-03-06 02:54:17 +0100

    Feat: Implements methods to update class Route and `Install ts-jest`. - Add methods: updatePostions, updateTilte and updatePoints. - Include tests for updatePostions, updateTilte and updatePoints.

---
+++ src/route.entity.spec.ts
@@ -0,0 +1,72 @@
+import exp from "constants";
+import { LatLng, Route, RouterProps } from "./route.entity";
+
+describe("Route Tests", () => {
+  test("Constructor", () => {
+    let routeProps: RouterProps = {
+      title: "My Route",
+      startPosition: { lat: 0, lng: 1 },
+      endPosition: { lat: 2, lng: 3 },
+    };
+    let route = new Route(routeProps);
+    expect(route.props).toStrictEqual({
+      ...routeProps,
+      points: [],
+    });
+
+    routeProps = {
+      title: "My Route",
+      startPosition: { lat: 0, lng: 1 },
+      endPosition: { lat: 2, lng: 3 },
+      points: [{ lat: 20, lng: 30 }],
+    };
+    route = new Route(routeProps);
+    expect(route.props).toStrictEqual({
+      ...routeProps,
+      points: [{ lat: 20, lng: 30 }],
+    });
+
+    route.updateTitle("My test");
+    expect(route.props.title).toBe("My test");
+  });
+
+  test("Should update the route title", () => {
+    const route: Route = new Route({
+      title: "My Route",
+      startPosition: { lat: 0, lng: 1 },
+      endPosition: { lat: 2, lng: 3 },
+      points: [{ lat: 20, lng: 30 }],
+    });
+    route.updateTitle("My test");
+    expect(route.title).toBe("My test");
+  });
+
+  test("Should update the route startPosition and endPosition", () => {
+    const route: Route = new Route({
+      title: "My Route",
+      startPosition: { lat: 0, lng: 1 },
+      endPosition: { lat: 2, lng: 3 },
+      points: [{ lat: 20, lng: 30 }],
+    });
+    const startPosition: LatLng = { lat: 10, lng: 20 };
+    const endPosition: LatLng = { lat: 30, lng: 40 };
+    route.updatePositions(startPosition, endPosition);
+    expect(route.startPosition).toBe(startPosition);
+    expect(route.endPosition).toBe(endPosition);
+    // expect(route.title).toBe("My test");
+  });
+
+  test("Should update the route points", () => {
+    const route = new Route({
+      title: "My Route",
+      startPosition: { lat: 20, lng: 30 },
+      endPosition: { lat: 40, lng: 50 },
+      points: [{ lat: 60, lng: 70 }],
+    });
+
+    const newPoints: LatLng[] = [{ lat: 10, lng: 22 }];
+    route.updatePoints(newPoints);
+    expect(route.points).toHaveLength(1);
+    expect(route.points).toStrictEqual(newPoints);
+  });
+});
--- src/route.entity.ts
+++ src/route.entity.ts
@@ -1,20 +1,77 @@
-type LatLng = {
+export type LatLng = {
   lat: number;
   lng: number;
 };
 
-type RouterProps = {
+export type RouterProps = {
   title: string;
   startPosition: LatLng;
   endPosition: LatLng;
-  points: LatLng[];
+  points?: LatLng[];
 };
 
-class Route {
-  constructor(private props: RouterProps) {}
+export class Route {
+  public props: Required<RouterProps>;
+  constructor(props: RouterProps) {
+    this.props = {
+      ...props,
+      points: props.points || [],
+    };
+  }
 
-  public set title(value: string) {
+  updateTitle(title: string) {
+    // All business logic should be here not in the Framework.
+    // Update to upperCase
+    // Validate characters
+    // Specific validations
+    this.title = title;
+  }
+
+  updatePositions(startPosition: LatLng, endPosition: LatLng) {
+    // All business logic should be here not in the Framework.
+    // Update to upperCase
+    // Validate characters
+    // Specific validations
+    this.startPosition = startPosition;
+    this.endPosition = endPosition;
+  }
+
+  updatePoints(value: LatLng[]) {
+    // All business logic should be here not in the Framework.
+    // Update to upperCase
+    // Validate characters
+    // Specific validations
+    this.points = value;
+  }
+
+  public get title(): string {
+    return this.props.title;
+  }
+
+  private set title(value: string) {
     this.props.title = value;
+  }
+
+  public get startPosition(): LatLng {
+    return this.props.startPosition;
+  }
+
+  private set startPosition(value: LatLng) {
+    this.props.startPosition = value;
+  }
+  public get endPosition(): LatLng {
+    return this.props.endPosition;
+  }
+
+  private set endPosition(value: LatLng) {
+    this.props.endPosition = value;
+  }
+  public get points(): LatLng[] {
+    return this.props.points;
+  }
+
+  private set points(value: LatLng[]) {
+    this.props.points = value;
   }
 }
 
